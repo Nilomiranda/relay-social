82becda21efba6d6c547b43ce6c392cf083ef0cc
Nilomiranda <nilomiranda3@gmail.com>
2020-03-21 15:06:51 -0300

    initiating react-native project

---
+++ packages/mobile/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+      <Text>Hi</Text>
+    </>
+  );
+};
+
+
+export default App;
